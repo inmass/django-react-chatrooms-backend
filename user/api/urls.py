6ab38775152ccb26f7574722fbf44c3cdb2d6a2b
inmass <inmass.idbel@gmail.com>
2023-02-06 00:00:31 +0100

    create api urls

--- user/api/urls.py
+++ user/api/urls.py
@@ -4,8 +4,9 @@ from .views import *
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path('', getRoutes, name="routes"),
     path('jwt/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('jwt/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterUserView.as_view(), name='register'),
+    path('rooms/', getRooms, name="rooms"),
+    path('create-room/', createRoom, name="create-room"),
 ]
--- user/api/views.py
+++ user/api/views.py
@@ -1,9 +1,11 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import generics
-from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated
+from .serializers import UserSerializer, RoomSerializer
+from user.models import Room
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
@@ -20,24 +22,32 @@ class CustomTokenObtainPairView(TokenObtainPairView):
 class RegisterUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
-@api_view(['POST'])
-def registerUser(request):
-    data = request.data
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getRooms(request):
 
-    username = data['username']
-    password = data['password']
-    password_confirmation = data['password_confirmation']
-    print(username, password, password_confirmation)
-        
+    # get all available rooms
+    rooms = Room.objects.all()
+    serializer = RoomSerializer(rooms, many=True)
+
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def createRoom(request):
+
+    # create a new room
+    try:
+        Room.objects.create(
+            name=request.data['name'],
+            description=request.data['description']
+        )
+        data = {
+            'detail': 'Room created successfully'
+        }
+    except:
+        data = {
+            'detail': 'Room creation failed'
+        }
 
     return Response(data)
-
-@api_view(['GET'])
-def getRoutes(request):
-
-    routes = {
-        'tokens':'api/token/',
-        'refresh':'api/token/refresh/',
-    }
-
-    return Response(routes)
